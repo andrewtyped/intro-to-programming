0f396b588b7edebb14b6b4e5889ea07ce310e0c0
Andrew Barger <asbarg01@gmail.com>
2017-09-20 20:31:29 -0400

    Remove code from initial version of lab. Add resources slides to presentation.

--- lab/src/initial/js/models.js
+++ lab/src/initial/js/models.js
@@ -15,23 +15,23 @@ function Point(x, y) {
 Point.prototype.normalize = function(worldSpaceRectangle, screenSpaceRectangle) {
 
     //Measure the world-space distance from the left-most edge to this point's x-coordinate.
-    const worldSpaceWidthOffset = this.X - worldSpaceRectangle.left;
+
 
     //Get the ratio of how wide the screen is vs. how wide the 'world' is.
-    const screenSpaceToWorldSpaceWidthRatio = screenSpaceRectangle.width / worldSpaceRectangle.width;
+
 
     //normalized x is world space width offset times the width ratio
-    const x = worldSpaceWidthOffset * screenSpaceToWorldSpaceWidthRatio;
+
 
     //Measure the world-space distance from the bottom edge to this point's y-coordinate
-    const worldSpaceHeightOffset = this.Y - worldSpaceRectangle.bottom;
+
 
     //Get the ratio of how tall the screen is vs. how tall the 'world is';
-    const screenSpaceToWorldSpaceHeightRatio = screenSpaceRectangle.height / worldSpaceRectangle.height;
+
 
     //normalized y is the screen height minus (world space height offset times height ratio).
     //We have to subtract from screen height b/c browsers consider (0,0) to be the UPPER left of the canvas.
-    const y = screenSpaceRectangle.height - (this.Y - worldSpaceRectangle.bottom) * screenSpaceRectangle.height / (worldSpaceRectangle.height);
 
-    return new Point(x, y);
+
+    //Return the new point to the caller
 }
--- lab/src/initial/js/point_parser.js
+++ lab/src/initial/js/point_parser.js
@@ -1,53 +1,41 @@
 function parsePoint(rawCoordinate) {
     //Validate the input - don't do anything if we weren't passed a value.
-    if (!rawCoordinate) {
-        return null;
-    }
+
 
     //Split the raw coordinate at the comma
-    const splitRawCoordinate = rawCoordinate.split(',');
+
 
     //If there wasn't exactly one comma, give up. The coordinate is malformed.
-    if (splitRawCoordinate.length !== 2) {
-        return null;
-    }
+
 
     //Parse each half of the coordinate into a real number
-    const x = parseFloat(splitRawCoordinate[0]);
-    const y = parseFloat(splitRawCoordinate[1]);
+
 
     //If either coordinate is not a number, give up. The coordinate is malformed.
-    if ((!x || !y) && !(x === 0 || y === 0)) {
-        return null;
-    }
+
 
     //Return the parsed point to the caller.
-    return new Point(x, y);
+
 }
 
 function parsePoints(rawCoordinates) {
 
     //Validate the input - Don't do anything if we weren't passed a value
-    if (!rawCoordinates) {
-        return [];
-    }
+
 
     //Split the user's input at each space to get an array of raw coordinates.
-    const splitRawCoordinates = rawCoordinates.split(' ');
+
 
     //Initialize an array to store the parsed coordinates.
-    const points = [];
+
 
     //Loop over the split raw coordinates
-    for (var i = 0; i < splitRawCoordinates.length; i++) {
+    //START LOOP
 
-        //Parse the raw coordinate
-        const point = parsePoint(splitRawCoordinates[i]);
+    //Parse the raw coordinate
 
-        //Add the parsed coordinate to the array
-        points.push(point);
-    }
+    //Add the parsed coordinate to the array
+    //END LOOP
 
     //Send the array of parsed coordinates back to the caller.
-    return points;
 }
